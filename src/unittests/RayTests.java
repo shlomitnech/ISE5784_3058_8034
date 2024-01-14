72fd51cde5329d0c530112e3a05dc2225c13c27d
Jessica Zemble <jessicafeinmesser@gmail.com>
2024-01-14 10:50:34 +0200

    ray test

--- src/unittests/RayTests.java
+++ src/unittests/RayTests.java
@@ -1,6 +1,7 @@
 package unittests;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import primitives.*;
 
 class RayTests {
     /*
@@ -8,5 +9,17 @@ class RayTests {
     *
 ▪ One boundary – zero distance
     * */
+    @Test
+    void testGetPoint() {
+        Ray testRay = new Ray(new Point(1,1,0), new Vector(1,2,3));
+        //EP 01: positive distance
+        assertEquals(new Point(1.5345224838248488,2.0690449676496976,1.6035674514745464), testRay.getPoint(2), "Error with positive distance");
+        //EP 02: negative distance
+        assertEquals(new Point(0.4654775161751512,-0.06904496764969759,-1.6035674514745464), testRay.getPoint(-2), "Error with negative distance");
+        //Boundary case: 0 distance
+        //assertNull(testRay.getPoint(0), "Error with positive distance");
+
+    }
 
 }
+
