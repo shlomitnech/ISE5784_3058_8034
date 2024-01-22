8c6fea19f8f2c336d150a13f63c907d3fd049efe
shlomitnech <shlomitbern@gmail.com>
2024-01-22 21:32:50 +0200

    integration test / camera

---
+++ src/scene/Scene.java
@@ -0,0 +1,58 @@
+package scene;
+
+import lighting.*;
+import primitives.*;
+import geometries.*;
+import primitives.Color;
+
+
+import java.awt.*;
+
+public class Scene {
+
+    public final String name;
+    public Color background = Color.BLACK;
+    public AmbientLight ambientLight = AmbientLight.NONE;
+    public Geometries geometries = new Geometries();
+
+    /***
+     * Constructor for Scene's name
+     * @param s
+     */
+    public Scene(String s) {
+        this.name = s;
+    }
+    /***
+     * Sets the background color
+     * @param c
+     * @return
+     */
+    public Scene setBackgroundColor( Color c) {
+        this.background = c;
+        return this;
+    }
+
+    /**
+     * sets the Ambient light to the color and scale given
+     * @param color
+     * @param ka
+     * @return
+     */
+    public Scene setAmbientLight(Color color, double ka) {
+        this.ambientLight = new AmbientLight(color, ka);
+        return this;
+    }
+    /**
+     * adds a geometry to the list
+     *
+     * @param geometries the Geometry you want to add to the scene
+     * @return the scene
+     */
+    public Scene setGeometries(Geometries... geometries) {
+        for (Intersectable g : geometries) {
+            this.geometries.add(g);
+        }
+        return this;
+    }
+
+}
