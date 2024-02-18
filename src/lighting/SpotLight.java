39a2fd0599f6f685f32c3877c6edf8204108f03a
shlomitnech <shlomitbern@gmail.com>
2024-02-18 21:28:12 +0200

    fixed blending issue

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -11,12 +11,23 @@ public class SpotLight extends PointLight {
         this.direction = direction.normalize();
     }
 
+    /**
+     *
+     * @param intensity
+     * @param position
+     * @param direction
+     */
     public SpotLight(Color intensity, Point position, Vector direction) {
         super(intensity, position);
         this.direction = direction.normalize();
     }
 
-
+    /**
+     * return the intensity
+     * use PointLight get Intensity * dotproduct of direction and point
+     * @param p
+     * @return
+     */
     public Color getIntensity(Point p) {
         return super.getIntensity(p).scale(Math.max(0, direction.dotProduct(getL(p))));
 
